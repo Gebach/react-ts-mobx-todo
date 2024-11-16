a32c6bd474f9cb1f58bcec572c241f836cb10ee5
Rodion <swizi_hs@mail.ru>
2024-11-16 21:17:30 +0500

    production

--- src/components/wrapper/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-interface WrapperProps {
-  children: React.ReactNode
-}
-
-function Wrapper({ children }: WrapperProps) {
-  return <div className="container max-w-5xl m-auto mt-5">{children}</div>
-}
-
-export default Wrapper
